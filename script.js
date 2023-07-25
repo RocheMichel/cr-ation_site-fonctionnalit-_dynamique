3773b55017cf4e480da18b5d493090f643581555
Roche <michel.roche@le-campus-numerique.fr>
2023-07-25 15:44:33 +0200

    enregistrement des dernière modification num 2

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+'use strict'
+/*------------------------------------------------------------------------------------------------------------*/
 /*Ici ce trouve la methode fetch pour permettre à ce que le feed puisse être visible sur le navigateur de la page d'acceuil */
 /*cette méthode en Java script permet d'obtenir des textes généré par un serveur qui va les intégrer à mon HTML */
-'use strict'
-
-        
+/*------------------------------------------------------------------------------------------------------------*/
+    
 fetch('https://v2.jokeapi.dev/joke/Any?lang=fr&amount=10')
     .then(r=>r.json())
     .then(articles =>{
@@ -17,12 +18,43 @@ fetch('https://v2.jokeapi.dev/joke/Any?lang=fr&amount=10')
             contener.appendChild(newdelivery)
         }
     })
-    
+
+/*------------------------------------------------------------------------------------------------------------*/
+/*C'est éléments me permet d'activer ou désactiver le menu déroulant celon si l'utilisateur click sur le menu burger*/
+/*------------------------------------------------------------------------------------------------------------*/
+
 const burger = document.querySelector (".menu_label"); 
     burger.addEventListener('click', () => ouvrire_menu());
-    
-   const menu = document.querySelector(".menu_deroulant");
+const menu = document.querySelector(".menu_deroulant");
+
 function ouvrire_menu () {
     menu.classList.toggle ('show')
+ }
+/*------------------------------------------------------------------------------------------------------------*/
+ /*Cette partie me permet de mettre en place une methode pour ajouter du contenue dans mon file d'actualité*/
+/*------------------------------------------------------------------------------------------------------------*/
+window.addEventListener("DOMContentLoaded", (e) => {
+    const delete1 = document.getElementById("delete1")
+        
+    delete1.addEventListener('click', () => {
+     const element1 = document.querySelector('article');
+     element1.removeChild(element1.children[0])
+    
+    })
+    function test(){
+        const contener = document.getElementById("contener_article")
+        
+            let titre_article= document.createElement('h2');
+            let contenue_article= document.createElement('p');
+            titre_article.textContent = document.getElementById("titre_article").value;
+            contenue_article.textContent = document.getElementById("texte_article").value;
+            contener.appendChild(titre_article)
+            contener.appendChild(contenue_article)
+        
+    }
 
-}
+    
+    const add_article = document.getElementById("add_article")
+    add_article.addEventListener('click', test)
+})
+
